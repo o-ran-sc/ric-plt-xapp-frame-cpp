08d209b487f888baa706b9fb58b55dcf5d597828
E. Scott Daniels <daniels@research.att.com>
2020-03-19 11:36:37 -0400

    Initial source commit
    This change contains the initial source for the framework.
    This is a work in progress, but should compile and produce
    both .deb and .rpm packages (with proper system support).
    
    Currently the support is for RMR based messaging only.
    
    The examples are rough but provide a first cut demonstration
    of the base framework.
    
    Issue-ID: RIC-148
    
    Signed-off-by: E. Scott Daniels <daniels@research.att.com>
    Change-Id: I9c64aa7db20c2977a4422d7257077e2531cc67d5

---
+++ src/messaging/callback.cpp
@@ -0,0 +1,62 @@
+// vi: ts=4 sw=4 noet:
+/*
+==================================================================================
+	Copyright (c) 2020 Nokia
+	Copyright (c) 2020 AT&T Intellectual Property.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================================
+*/
+
+/*
+	Mnemonic:	callback.cpp
+	Abstract:	Function defs for the callback managment object
+	Date:		9 March 2020
+	Author:		E. Scott Daniels
+*/
+
+#include <cstdlib>
+
+#include <rmr/rmr.h>
+
+#include "message.hpp"
+//class Messenger;
+
+
+/*
+	Builder.
+*/
+Callback::Callback( user_callback ufun, void* data ) {		// builder
+	user_fun = ufun;
+	udata = data;
+}
+
+/*
+	Wrecking ball.
+*/
+Callback::~Callback() {
+	// nothing to do at the moment.
+}
+
+/*
+	Drive_cb will invoke the callback and pass along the stuff passed here.
+*/
+void Callback::Drive_cb( Messenger& mr, Message& m ) {
+	if( user_fun == NULL ) {
+		return;
+	}
+
+	user_fun( mr, m, m.Get_mtype(), m.Get_subid(), m.Get_len(), m.Get_payload(),  udata );
+}
+
+
