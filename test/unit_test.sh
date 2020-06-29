c725a9a3d62d62ca2109b5288012cd63a23d6a15
E. Scott Daniels <daniels@research.att.com>
2020-06-29 15:39:57 -0400

    Add json support
    The munchkin xAPP needs lightweight json, as might other xAPPs,
    so it makes sense to do the work onece and add it to the framework.
    
    Issue-ID: RIC-420
    
    Signed-off-by: E. Scott Daniels <daniels@research.att.com>
    Change-Id: I20ea999ba77566d1404f0d69be0f63669943236e

--- test/unit_test.sh
+++ test/unit_test.sh
@@ -30,14 +30,14 @@
 
 # Make a list of our modules under test so that we don't look at gcov
 # files that are generated for system lib headers in /usr/*
-# (bash makes the process of building a list of names  harder than it 
+# (bash makes the process of building a list of names  harder than it
 # needs to be, so use caution with the printf() call.)
 #
 function mk_list {
 	grep -l "Source:\.\./src"  *.gcov | while read f
 	do
 		printf "$f "		# do NOT use echo or add \n!
-	done 
+	done
 }
 
 function abort_if_error {
@@ -70,18 +70,30 @@ do
 	shift
 done
 
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+export LIBRARY_PATH=/usr/local/lib:$LIBRARY_PATH
+
 make nuke >/dev/null
-make unit_test >/tmp/PID$$.log 2>&1
+make tests >/tmp/PID$$.log 2>&1
 abort_if_error $? "unable to make"
+echo "tests successfully built" >&2
 
 spew="cat"
-./unit_test >/tmp/PID$$.log 2>&1
-abort_if_error $? "unit test failed"
 
-gcov unit_test >/tmp/PID$$.gcov_log 2>&1	# suss out our gcov files
+for x in unit_test jhash_test
+do
+	./$x >/tmp/PID$$.log 2>&1
+	abort_if_error $? "test failed: $x"
+	gcov $x.c >/dev/null 2>&1
+done
+
+# wrapper_test is driven by jhash_test, but must be covered explicitly
+gcov jwrapper_test.c >/dev/null 2>&1
+
 ./scrub_gcov.sh								# remove cruft
 
 list=$( mk_list )
+echo ""
 echo "[INFO] coverage stats, discounted (raw), for the various modules:"
 ./parse_gcov.sh $list						# generate simple, short, coverage stats
 
