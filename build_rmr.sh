52b8fb21b577438bdce8658b60a27d6379d713f2
wahidw <abdulwahid.w@nokia.com>
2021-06-23 11:25:09 +0000

    Updating RMR to v4.7.4
    Change-Id: I537dfac9bfb08856cad87d037d9b164947ead227
    Signed-off-by: wahidw <abdulwahid.w@nokia.com>

--- build_rmr.sh
+++ build_rmr.sh
@@ -6,7 +6,7 @@
 #				repo and installing it.  The package method is preferred
 #				but if that breaks this can be used in place of it.
 
-rmr_ver=${1:-4.4.1}
+rmr_ver=${1:-4.7.4}
 
 # assume that we're in the proper directory
 set -e
