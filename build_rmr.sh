430b0914167f4c4bcf56ee476133a88a7f64ee3a
czichy <thoralf.czichy@nokia.com>
2023-06-08 14:48:50 +0300

    RMR update and H release step 1 of 2
    Change-Id: I9443d5dc6434d6988e1bcca72513ffd45751714f
    Signed-off-by: czichy <thoralf.czichy@nokia.com>

--- build_rmr.sh
+++ build_rmr.sh
@@ -6,7 +6,7 @@
 #				repo and installing it.  The package method is preferred
 #				but if that breaks this can be used in place of it.
 
-rmr_ver=${1:-4.8.5}
+rmr_ver=${1:-4.9.1}
 
 # assume that we're in the proper directory
 set -e
