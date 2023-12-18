31b1f9e26ed8609d9c996ff01d5c46bb036cbf15
Tuan Nguyen <hoang.su.tk@gmail.com>
2023-12-18 05:53:54 +0000

    RIC-1033: Add method for xapp registraiton to xapp-frame-c
    Change-Id: I6e0ff1c60c53573ff737c9c8e32061d09664c0d4
    Signed-off-by: Tuan Nguyen <hoang.su.tk@gmail.com>

--- src/xapp/xapp.cpp
+++ src/xapp/xapp.cpp
@@ -28,23 +28,15 @@
 	Author:		E. Scott Daniels
 */
 
-#include <string.h>
+#include <algorithm>
 #include <unistd.h>
-
-#include <rmr/rmr.h>
-#include <rmr/RIC_message_types.h>
-
-
-#include <iostream>
 #include <thread>
 
-#include <messenger.hpp>
+#include <rmr/RIC_message_types.h>
+#include "RestClient.h"
 #include "xapp.hpp"
-
-// --------------- private -----------------------------------------------------
-
-
-
+using json = nlohmann::json;
+using namespace std;
 // --------------- builders -----------------------------------------------
 
 /*
@@ -54,8 +46,13 @@
 
 	If port is nil, then the default port is used (4560).
 */
-Xapp::Xapp( const char* port, bool wait4table ) : Messenger( port, wait4table ) {
-	// nothing to do; all handled in Messenger constructor
+Xapp::Xapp( const char* port, bool wait4table ): Messenger(port, false) {
+	thread regThread(&Xapp::RegisterXapp, this);
+	regThread.detach();
+
+	if( wait4table ) {
+		this->Wait_for_cts( 0 );
+	}
 }
 
 /*
@@ -73,12 +70,12 @@ Xapp::~Xapp() {
 */
 void Xapp::Run( int nthreads ) {
 	int i;
-	std::thread** tinfo;				// each thread we'll start
+	thread** tinfo;				// each thread we'll start
 
-	tinfo = new std::thread* [nthreads-1];
+	tinfo = new thread* [nthreads-1];
 
 	for( i = 0; i < nthreads - 1; i++ ) {				// thread for each n-1; last runs here
-		tinfo[i] = new std::thread( &Xapp::Listen, this );
+		tinfo[i] = new thread( &Xapp::Listen, this );
 	}
 
 	this->Listen();						// will return only when halted
@@ -98,3 +95,111 @@ void Xapp::Halt() {
 	this->Stop();			// messenger shut off
 }
 
+string Xapp::GetService(string host, const char* service) {
+	string service_url = "";
+	string app_namespace = (getenv("APP_NAMESPACE") == nullptr) ? "ricxapp" : string(getenv("APP_NAMESPACE"));
+
+	if (host != "") {
+		transform(host.begin(), host.end(), host.begin(), ::toupper);
+		transform(app_namespace.begin(), app_namespace.end(), app_namespace.begin(), ::toupper);
+
+		char buffer[100];
+		sprintf(buffer, service, app_namespace.c_str(), host.c_str());
+
+		string endpoint_var = string(buffer);
+		replace(endpoint_var.begin(), endpoint_var.end(), '-', '_');
+
+		char* env_value = getenv((char*)&endpoint_var);
+
+		char* token = strtok((char*)&env_value, "//");
+
+		if (token != NULL) {
+			token = strtok(NULL, "//");
+		}
+
+		service_url = string(token);
+	}
+
+	return service_url;
+}
+        
+
+bool Xapp::Register() {
+	string hostname = string(getenv("HOSTNAME"));
+	string xappname = config.Get_control_str("name");
+	string xappversion = config.Get_control_str("version");
+
+	string pltnamespace = (getenv("PLT_NAMESPACE") == nullptr) ? "ricplt" : string(getenv("PLT_NAMESPACE"));
+
+	string http_endpoint = this->GetService(hostname, SERVICE_HTTP);
+	string rmr_endpoint = this->GetService(hostname, SERVICE_RMR);
+
+	cout	<< "config details hostname : " << hostname
+				<< ", xappname: " << xappname
+				<< ", xappversion : " << xappversion
+				<< "pltnamespace : " << pltnamespace
+				<< ", http_endpoint: " << http_endpoint
+				<< ", rmr_endpoint: " << rmr_endpoint
+				<< endl;
+
+	json* request_string = new json
+		{
+			{"appName", hostname},
+			{"appVersion", xappversion},
+			{"configPath", CONFIG_PATH},
+			{"appInstanceName", xappname},
+			{"httpEndpoint", http_endpoint},
+			{"rmrEndpoint", rmr_endpoint},
+			{"config", config.Get_contents()}
+
+		};
+
+	cout << "REQUEST :" << request_string->dump() << endl;
+
+	// sending request
+	
+
+	char buffer[100];
+	sprintf(buffer, REGISTER_PATH, pltnamespace.c_str(), pltnamespace.c_str());
+	string register_url = string(buffer);
+	xapp::cpprestclient client(register_url);
+
+	const json& const_request_string = *request_string;
+
+	xapp::response_t resp = client.do_post( const_request_string, "" ); // we already have the full path in ts_control_ep
+
+	if( resp.status_code == 200 ) {
+		// ============== DO SOMETHING USEFUL HERE ===============
+		// Currently, we only print out the HandOff reply
+		rapidjson::Document document;
+		document.Parse( resp.body.dump().c_str() );
+		rapidjson::StringBuffer s;
+		rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(s);
+		document.Accept( writer );
+		cout << "[INFO] HandOff reply is " << s.GetString() << endl;
+		return 1;
+	} else {
+		cout << "[ERROR] Unexpected HTTP code " << resp.status_code << " from " << \
+				client.getBaseUrl() << \
+				"\n[ERROR] HTTP payload is " << resp.body.dump().c_str() << endl;
+	}
+
+	return 0;
+	
+}
+
+/*
+	Register the xapp with Appmgr. 
+*/
+void Xapp::RegisterXapp() {
+	bool isReady = false;
+
+	while (not isReady) {
+		sleep(5);
+		if (this->Register()) {
+			cout<<"Registration done, proceeding with startup ..."<<endl;
+			break;
+		}
+		
+	}
+}
